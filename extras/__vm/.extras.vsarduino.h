b9ee6e80f20f7a3368021ab900940314136d71a0
FancyFoxGems <tjbtech@yahoo.com>
2016-10-02 20:27:47 -0700

    asdf

--- extras/__vm/.extras.vsarduino.h
+++ extras/__vm/.extras.vsarduino.h
@@ -1,10 +1,10 @@
-/* 
+/*
 	Editor: http://www.visualmicro.com
 	        visual micro and the arduino ide ignore this code during compilation. this code is automatically maintained by visualmicro, manual changes to this file will be overwritten
 	        the contents of the Visual Micro sketch sub folder can be deleted prior to publishing a project
 	        all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
 	        note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
-	
+
 	Hardware: Arduino Nano w/ ATmega328, Platform=avr, Package=arduino
 */
 
@@ -24,7 +24,7 @@
 #define __inline__
 #define __volatile__
 #define GCC_VERSION 40801
-#define volatile(va_arg) 
+#define volatile(va_arg)
 #define _CONST
 #define __builtin_va_start
 #define __builtin_va_end
@@ -45,10 +45,12 @@ extern "C" void __cxa_pure_virtual() {;}
 
 
 #include <arduino.h>
-#include <pins_arduino.h> 
+#include <pins_arduino.h>
 #undef F
 #define F(string_literal) ((const PROGMEM char *)(string_literal))
 #undef PSTR
 #define PSTR(string_literal) ((const PROGMEM char *)(string_literal))")
 #include "HalfStepper.ino"
+#include <HalfStepper\HalfStepper.cpp>
+#include <HalfStepper\HalfStepper.h>
 #endif
