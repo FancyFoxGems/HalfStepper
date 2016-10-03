1b7d4bd9637ba3a7ccd28116d7e4de9a2a88042f
FancyFoxGems <tjbtech@yahoo.com>
2016-10-02 20:46:59 -0700

    asdf

--- extras/__vm/.HalfStepper.vsarduino.h
+++ extras/__vm/.HalfStepper.vsarduino.h
@@ -1,10 +1,10 @@
-/*
+/* 
 	Editor: http://www.visualmicro.com
 	        visual micro and the arduino ide ignore this code during compilation. this code is automatically maintained by visualmicro, manual changes to this file will be overwritten
 	        the contents of the Visual Micro sketch sub folder can be deleted prior to publishing a project
 	        all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
 	        note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
-
+	
 	Hardware: Arduino Nano w/ ATmega328, Platform=avr, Package=arduino
 */
 
@@ -24,7 +24,7 @@
 #define __inline__
 #define __volatile__
 #define GCC_VERSION 40801
-#define volatile(va_arg)
+#define volatile(va_arg) 
 #define _CONST
 #define __builtin_va_start
 #define __builtin_va_end
@@ -40,17 +40,31 @@
 #endif
 #define NEW_H
 typedef void *__builtin_va_list;
+typedef unsigned char byte;
 extern "C" void __cxa_pure_virtual() {;}
 
 
 
 #include <arduino.h>
-#include <pins_arduino.h>
+#include <pins_arduino.h> 
 #undef F
 #define F(string_literal) ((const PROGMEM char *)(string_literal))
 #undef PSTR
-#define PSTR(string_literal) ((const PROGMEM char *)(string_literal)))
-#include <..\..\..\HalfStepper.ino>
-#include <..\..\HalfStepper\HalfStepper.cpp>
-#include <..\..\HalfStepper\HalfStepper.h>
+#define PSTR(string_literal) ((const PROGMEM char *)(string_literal))
+#undef cli
+#define cli()
+#define pgm_read_byte(address_short)
+#define pgm_read_word(address_short)
+#define pgm_read_word2(address_short)
+#define digitalPinToPort(P)
+#define digitalPinToBitMask(P) 
+#define digitalPinToTimer(P)
+#define analogInPinToBit(P)
+#define portOutputRegister(P)
+#define portInputRegister(P)
+#define portModeRegister(P)
+#include <..\HalfStepper\HalfStepper.ino>
+#include <HalfStepper\examples\BasicTest\BasicTest.ino>
+#include <HalfStepper\HalfStepper.cpp>
+#include <HalfStepper\HalfStepper.h>
 #endif
