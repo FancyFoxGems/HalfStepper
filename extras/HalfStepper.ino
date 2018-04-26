582d45fdd664577b5505a19aa908bc6cc733919c
FancyFoxGems <tjbtech@yahoo.com>
2018-04-26 12:34:19 -0700

    asdf

--- extras/HalfStepper.ino
+++ extras/HalfStepper.ino
@@ -1,1 +1,9 @@
-#include "HalfStepper.h"
+/***********************************************************************************************
+* This file is part of the HalfStepper Arduino library.                                        *
+* Copyright © 2016 Thomas J. Biuso III  ALL RIGHTS RESERVED...WHATEVER THAT MEANS.             *
+* RELEASED UNDER THE LGPL v3.0 LICENSE; SEE <LICENSE> FILE WITHIN DISTRIBUTION ROOT FOR TERMS. *
+************************************************************************************************/
+
+void setup() { }
+
+void loop() { }
